2c65b1415d76a17f53f72dc6c17bb84491b48320
Ram ratan yadav <rratan100@gmail.com>
2024-12-09 18:58:30 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Example AMI ID
+  instance_type = "t2.micro"
 }
 
 module "vpc" {
