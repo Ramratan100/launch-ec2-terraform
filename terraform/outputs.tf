0ea2c99bb58e658df209c826ea9f31f485b2a056
Ram ratan yadav <rratan100@gmail.com>
2024-12-09 15:13:14 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "instance_public_ip" {
-  value = aws_instance.web_server.public_ip
+output "apache_instance_public_ip" {
+  value = aws_instance.apache_instance.public_ip
 }
 
-output "instance_public_dns" {
-  value = aws_instance.web_server.public_dns
+output "mysql_instance_private_ip" {
+  value = aws_instance.mysql_instance.private_ip
 }
