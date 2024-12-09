6f7e7ef50b0b102f1d4143616e1dec20ed2a3e2d
Ram ratan yadav <rratan100@gmail.com>
2024-12-09 15:12:32 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,25 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "vpc_name" {
+  default = "launch-ec2-vpc"
+}
+
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+}
+
 variable "instance_type" {
-  description = "Type of EC2 instance"
-  default     = "t2.micro"
+  default = "t2.micro"
 }
 
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
+variable "apache_ami" {
+  description = "AMI for Apache Web Server"
+  default     = "ami-0c55b159cbfafe1f0"  # Update with correct Apache AMI
 }
 
-variable "key_name" {
-  description = "The SSH key name for the EC2 instance"
+variable "mysql_ami" {
+  description = "AMI for MySQL Database"
+  default     = "ami-0c55b159cbfafe1f0"  # Update with correct MySQL AMI
 }
