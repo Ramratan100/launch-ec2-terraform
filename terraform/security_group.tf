ab2326904918072e0a8517f51cd46630cf144060
Ram ratan yadav <rratan100@gmail.com>
2024-12-09 15:11:43 +0530

    Update security_group.tf

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,13 +1,8 @@
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow inbound HTTP, HTTPS, and SSH traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Apache Security Group allows HTTP and SSH access from the public internet
+resource "aws_security_group" "apache_sg" {
+  name        = "apache_sg"
+  description = "Allow Apache traffic"
+  vpc_id      = module.vpc.vpc_id
 
   ingress {
     from_port   = 80
@@ -17,10 +12,31 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# MySQL Security Group allows traffic only from the Apache server
+resource "aws_security_group" "mysql_sg" {
+  name        = "mysql_sg"
+  description = "Allow MySQL traffic from Apache"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]  # Allow access from within the VPC
   }
 
   egress {
